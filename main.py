4bad10c61de8d1c3b7db864eb17109d685ceb749
sravankumarchinni <46323052+sravankumarchinni@users.noreply.github.com>
2021-01-11 00:32:25 +0530

    hai

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ from cinema import Cinema
 
 row = int(input('Enter the number of rows:\n'))
 seats = int(input("Enter the number of seats in each row:\n"))
-while(True):
-    ch = input("1. Show the seats\n2. Buy a ticket\n3. Statistics\n4. Show booked Tickets User Info\n0. Exit\n")
-    if ch == "1":
-        obj = Cinema()
-        obj.show_seats(row,seats)
-
-    if ch == "2":
-        obj = Cinema()
-        obj.buy_ticket(row,seats)
-
-    if ch == "3":
-        obj = Cinema()
-        obj.show_statistics(row,seats)
-
-    if ch == "4":
-        obj = Cinema()
-        obj.show_booked_ticket()
-
-    if ch == "0":
-        obj = Cinema()
-        obj.exit_method()
-        break
-
+if __name__ == "__main__":
+	choice = 1
+	while choice != 7:
+		print('\n=========WELCOME TO BOOK YOUR MOVIE==========')
+		print('-----------------------------------------')
+		print('1. Show the seats')
+		print('2. Buy a Ticket')
+		print('3. Stastics')
+		print('4. Show Booked Bickets user Info')
+		print('0. Exit')
+		
+		choice = int(input('ENTER YOUR CHOICE: '))
+		if choice == 1:
+			show_seats()
+		elif choice == 2:
+			buy_ticket()
+		elif choice == 3:
+			show_statistics()
+		elif choice == 4:
+			show_booked_ticket()
+		elif choice == 0:
+			Exit()
+		else:
+			print('WRONG INPUT. ENTER THE CHOICE AGAIN')
 
 
